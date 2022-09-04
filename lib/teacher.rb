78bc7e425ece31ce16dcd1a7d4ed68f2ac700949
Simeon <olabs06@yahoo.com>
2022-09-04 18:02:26 +0100

    creating inheritances

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,15 @@
-class Teacher
+require_relative './user'
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+  def teach
+    KNOWLEDGE[randomizer(KNOWLEDGE)]
+  end
+
+  private
+  def randomizer array
+    rng = Random.new
+    rng.rand(array.length)
+  end   
 end
